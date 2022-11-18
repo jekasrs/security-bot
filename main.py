bc2e26bf9ba36342f1f5e884fee18d6c8ca5cfb9
Evgeny Smirnov <90210620+jekasrs@users.noreply.github.com>
2022-11-18 19:31:31 +0300

    security mode created

--- main.py
+++ main.py
@@ -154,11 +154,12 @@
 # #     os.popen("ffmpeg -y -i 'res.avi' -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 'res.mp4'".format(input = '/home/pi/project/res.avi', output = '/home/pi/project/res.mp4'))
 #
 
-
 import telebot
 import time
 import os
 from PIL import Image
+import numpy as np
+from datetime import datetime
 
 from telebot import types
 import moviepy.editor as mp
@@ -171,6 +172,8 @@ URL = 'https://api.telegram.org/bot'
 bot = telebot.TeleBot(TOKEN)
 keyboard = types.InlineKeyboardMarkup()
 
+backSub = cv2.createBackgroundSubtractorMOG2(50, 16, True)
+stop_track = False
 
 def make_video():
     stream = cv2.VideoCapture(0)
@@ -209,37 +212,55 @@ def make_video():
 
 
 def start_security(id):
-    # if noticed:
-    path = make_video()
-    bot.send_message(id, 'Attantion! Enime: detected')
-    bot.send_video(id, open(path, 'rb'), None, 'Text')
+    global stop_track
+    stream = cv2.VideoCapture(0)
+    frame_width = int(stream.get(3))
+    frame_height = int(stream.get(4))
+    stream.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    stream.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    stop_track = True
 
+    while stop_track:
 
-def make_GIF():
-    pass
-    # Список для хранения кадров.
-    # frames = []
+        # шаг 1: получить изображение
+        _, frame = stream.read()
 
-    # for frame_number in range(1, 5):
-    #    # Открываем изображение каждого кадра.
-    #    frame = Image.open(f'~/project/pictures-{frame_number}.jpg')
-    #    frame = frame.rotate(270)
-    #    # Добавляем кадр в список с кадрами.
-    #    frames.append(frame)
+        # шаг 2: вычитание фона
+        fg_mask = backSub.apply(frame)
 
-    ## Берем первый кадр и в него добавляем оставшееся кадры.
-    # frames[0].save(
-    #    '/Users/evgenijsmirnov/PycharmProjects/SecurityBot/result-gifs/home.gif',
-    #    save_all=True,
-    #    append_images=frames[1:],  # Срез который игнорирует первый кадр.
-    #    optimize=True,
-    #    duration=100,
-    #    loop=0
-    # )
+        # шаг 3: бинаризация маски
+        _, mask_thr = cv2.threshold(fg_mask, 100, 255, 0)  # с тенями
 
-    # clip = mp.VideoFileClip("/Users/evgenijsmirnov/PycharmProjects/SecurityBot/result-gifs/home.gif")
-    # clip.write_videofile("/Users/evgenijsmirnov/PycharmProjects/SecurityBot/result-gifs/home.mp4")
+        # шаг 4: исключение мелкого шума
+        kernel_open = np.ones((5, 5), np.uint8)
+        mask_open = cv2.morphologyEx(mask_thr, cv2.MORPH_OPEN, kernel_open)
 
+        # шаг 5: исключение мелких областей
+        kernel_close = np.ones((9, 9), np.uint8)
+        mask_close = cv2.morphologyEx(mask_open, cv2.MORPH_CLOSE, kernel_close)
+
+        # шаг 6: поиск контуров
+        contours, _ = cv2.findContours(mask_close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # шаг 7: исключение контуров малой площади
+        area_threshold = 100
+        contours_sel = [cnt for cnt in contours if cv2.contourArea(cnt) > area_threshold]
+
+        # шаг 8: расчет площади контуров
+        total_area = 0
+        for cnt in contours_sel:
+            total_area += cv2.contourArea(cnt)
+        rel_area = total_area / (frame.shape[0] * frame.shape[1]) * 100
+
+        # шаг 9: проверка движения в кадре
+        motion_threshold = 0.5
+
+        if rel_area > motion_threshold:
+            # cv2.destroyAllWindows()
+            # stream.release()
+            path = make_video()
+            bot.send_message(id, 'Attantion! Enime: detected')
+            bot.send_video(id, open(path, 'rb'), None, 'Text')
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
@@ -296,11 +317,13 @@ def stop_tracking(message):
 
 @bot.message_handler(content_types=['text'])  # обработка текстового сообщения
 def random_answers(message):
+    global stop_track
     if message.text == 'Сделать фото':
         get_picture(message)
     if message.text == 'Начать отслеживание':
         start_tracking(message)
     if message.text == 'Завершить слежку':
+        stop_track = False
         answer = 'Отслеживание завершено!'
         bot.send_message(message.chat.id, answer)
         back(message)
